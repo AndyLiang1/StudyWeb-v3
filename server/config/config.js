77254eeaac67eb6ff1f88221e551da196ee0be3b
AndyLiang1 <andyliang1000@gmail.com>
2021-09-29 19:36:48 -0700

    Prod docker file.

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   "development": {
-    "username": process.env.DEV_DB_USERNAME,
-    "password": process.env.DEV_DB_PASSWORD,
-    "database": process.env.DEV_DB_DATABASE,
-    "host": process.env.DEV_DB_HOST,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": 'mysql', // dialect has to be explictly supplied as of v4.0.0
   },
   "test": {
